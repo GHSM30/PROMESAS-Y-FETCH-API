c000e444be72e2ec27acd8dcd54c93b8091dd212
Guillermo Hilario Santos Moya <guhisantosmo@ittepic.edu.mx>
2024-09-11 20:41:21 -0700

    AppPWA

---
+++ AppPWAJSAllModules/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+//import App from './App.jsx'
+import './index.css'
+//import MyFunctions from './functions/MyFunctions.jsx'
+//import MyPromises from './functions/MyPromises.jsx'
+//import MyPromisesAll from './functions/MyPromisesAll.jsx'
+//import MyPromisesRace from './functions/MyPromisesRace'
+//import MyFetchsAPIs from './functions/MyFetchs'
+//import MyFetchPost from './functions/MyFetchPost'
+import MyFetchPut from './functions/MyFetchPut'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/*<App />*/}
+    {/* <MyFunctions/> */}
+    {/* <MyPromises/> */}
+    {/* <MyPromisesAll/> */}
+    {/* <MyPromisesRace/> */}
+    {/* <MyFetchsAPIs/>*/} 
+    {/* <MyFetchPost/>*/} 
+    <MyFetchPut/>
+  </StrictMode>,
+)
